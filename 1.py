067cd47c9ea99c2388fd7668339feaf2b4b46687
Sergey Kozlov <skozlovf@gmail.com>
2016-06-17 17:51:50 +0600

    Update API documentation with autodoc
    1:1: [python:D001] missing docstring: foo
    5:1: [python:D001] missing docstring: bla
    9:1: [python:D001] missing docstring: blasss

--- 1.py
+++ 1.py
@@ -1,12 +1,29 @@
 def foo(a, b):
+    """
+
+    Args:
+        a:
+        b:
+    """
     pass
 
 
 def bla(xxx):
+    """
+
+    Args:
+        xxx:
+
+    Returns:
+
+    """
     return 1
     
     
 def blasss():
+    """
+
+    """
     pass
 
 
